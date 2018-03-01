2214bf005c99724560099b51ec2ee3795b60db81
Christian Paun <christianpaun@gmail.com>
2018-03-01 09:25:47 +0000

    more aux containers

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './Person.css';
+import withClass from '../../../hoc/WithClass';
+import Aux from '../../../hoc/Aux';
 
 class Person extends Component {
     constructor( props ) {
@@ -19,13 +22,25 @@ class Person extends Component {
     render () {
         console.log( '[Person.js] Inside render()' );
         return (
-            <div className={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            </Aux>
         )
+        // return [
+        //     <p key="1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
+        //     <p key="2">{this.props.children}</p>,
+        //     <input key="3" type="text" onChange={this.props.changed} value={this.props.name} />
+        // ]
     }
 }
 
-export default Person;
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func 
+};
+
+export default withClass(Person, classes.Person);
